27d841ef88df76a431328b4f6b3fc2f5e0b2a24b
juanmderosa <juanmderosa@gmail.com>
2025-10-21 10:32:15 -0300

    feat: Configuraciones iniciales y página de inicio.

---
+++ src/lib/swiper.js
@@ -0,0 +1,22 @@
+import Swiper from "swiper";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper-hero", {
+    modules: [Navigation, Autoplay],
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
